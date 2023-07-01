51dd6be3a3c6a3e6611b12f9c20058c2fd341d0f
Frederick Asante <dark.job1995@gmail.com>
2023-07-01 02:55:13 +0000

    Add files via upload
    Estate Web Application | Creating various razor UI components out of Template

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BlazorWasm.AffordableHomes.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWasm.AffordableHomes.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<House> Houses { get; set; } = default!;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using BlazorWasm.AffordableHomes.Server.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Database connection string 'DefaultConnection' doest not exist"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
+
